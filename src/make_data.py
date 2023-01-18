f03a64bf91244ad5d42a5c95751fb18644aad7c3
Hedriss10 <hedrisgts@gmail.com>
2023-01-18 15:56:30 -0300

    "SQL"

---
+++ src/make_data.py
@@ -0,0 +1,24 @@
+import sqlalchemy
+import pandas as pd 
+import os 
+import sys 
+
+sys.path.append('..')
+
+
+filepath =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+data = os.path.join(filepath , 'data')
+base_db = os.path.join(data, 'bank.csv')
+
+try:
+    df = pd.read_csv( base_db , sep=';')
+    table_name = 'tb_base'
+    string_conetion = 'sqlite:///{path}'
+    string_conetion = string_conetion.format( path=os.path.join(data, 'base.db') )
+    conection = sqlalchemy.create_engine( string_conetion )
+    df.to_sql( name=table_name, con=conection, if_exists='replace' )
+    
+    
+    
+except Exception as e:
+    print(e)
